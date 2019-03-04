3760a297a529ffa63cdbb9e236677fbae1486991
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-04 04:57:38 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,11 @@
 class Project
   attr_reader :backers
+  
   def initialize(title)
     @title = title
     @backers = []
   end
+  
   def add_backer(backer)
     @backers << backer
     backer.back_project(self)
